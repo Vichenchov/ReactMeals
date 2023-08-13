f2909fe6561888141f256d871f1fc4b34278507f
Vichenchov <max.viva10@gmail.com>
2023-08-13 16:37:33 +0300

    working on cart...

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState, Fragment} from 'react'
 import UpperBar from './components/UpperBar/UpperBar'
 import Img from './resources/img/cactus-jack-x-mcdonalds.jpg'
 import Description from './components/Description/Description'
@@ -5,11 +6,22 @@ import Menu from './components/Menu/Menu'
 import Cart from './components/Cart/Cart'
 
 function App() {
+  const [cartIsShown,setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
+
   return (
-    <>
+    <Fragment>
       <div className="mainDiv">
-        <Cart/>
-        <UpperBar title="ReactMeals" />
+        {cartIsShown && <Cart/>}
+        <UpperBar title="ReactMeals" onShowCart={showCartHandler} />
         <div className="diagonal-crop">
           <img src={Img} className="imgSize diagonal-crop" />
         </div>
@@ -20,7 +32,7 @@ function App() {
           <Menu />
         </div>
       </div>
-    </>
+    </Fragment>
   )
 }
 
